90c914b4bd6d745c552df11c907e39981e5dec74
EduardovJS <eduardoviniciusjorge@gmail.com>
2024-12-09 22:02:28 -0300

    projeto iniciado

---
+++ area-adm/Areas/Adm/Controllers/AdmController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace area_adm.Areas.Adm.Controllers
+{
+
+    [Area("Adm")]
+    public class AdmController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
