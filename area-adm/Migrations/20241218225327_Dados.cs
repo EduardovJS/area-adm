ff2d0cfec04c1242135c484afab0e99bed0c2856
EduardovJS <eduardoviniciusjorge@gmail.com>
2024-12-18 21:20:56 -0300

    Tela de Login

---
+++ area-adm/Migrations/20241218225327_Dados.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace area_adm.Migrations
+{
+    /// <inheritdoc />
+    public partial class Dados : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
